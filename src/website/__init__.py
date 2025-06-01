6c52a534e5ad8f22485e46e9768f08065580b97b
l0vpreslava <l0vpreslava@gmail.com>
2025-06-01 17:00:23 +0300

    Configure jinja temlates

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -15,4 +15,31 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def handle_root(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+    return templates.TemplateResponse(request=request, name="pages/app/index.html")
+
+
+@app.get("/commissions", response_class=HTMLResponse)
+def handle_commissions(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="pages/app/commissions.html"
+    )
+
+
+@app.get("/gallery", response_class=HTMLResponse)
+def handle_gallery(request: Request):
+    return templates.TemplateResponse(request=request, name="pages/app/gallery.html")
+
+
+@app.get("/oc", response_class=HTMLResponse)
+def handle_oc(request: Request):
+    return templates.TemplateResponse(request=request, name="pages/app/oc.html")
+
+
+@app.get("/login", response_class=HTMLResponse)
+def handle_login(request: Request):
+    return templates.TemplateResponse(request=request, name="pages/admin/login.html")
+
+
+@app.get("/admin", response_class=HTMLResponse)
+def handle_admin(request: Request):
+    return templates.TemplateResponse(request=request, name="pages/admin/index.html")
